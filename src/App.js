06b20f8657a24ffa8d3b12ae6e34ea913cbdf41c
mFaridun <musirmonovfaridun@gmail.com>
2023-05-06 09:25:44 +0500

    new code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Lesson02 from './Companent/Lesson02/Lesson02'
+import ExampleClass from './Companent/Lesson02/Lesson02';
 
 function App() {
   return (
     <div className="App">
-      <Lesson02 />
+      {/* <Lesson02 /> */}
+      <ExampleClass />
     </div>
   );
 }
--- src/Companent/Lesson02/Lesson02.jsx
+++ src/Companent/Lesson02/Lesson02.jsx
@@ -1,37 +1,70 @@
-import { useState } from "react";
-import './Lesson02.css'
+// import { useState } from "react";
+// import './Lesson02.css'
 
-const Lesson02 = (props) => {
+// const Lesson02 = (props) => {
 
-  const [count, setCount] = useState(1);
-  const [dark, setDark] = useState(false);
+//   const [count, setCount] = useState(1);
+//   const [dark, setDark] = useState(false);
 
-  const handleCount = () => {
-    setCount(count + 1);
+//   const handleCount = () => {
+//     setCount(count + 1);
+//   }
+
+//   const on = () => {
+//     setCount(count - 1);
+//   }
+
+//   const darkNot = () => {
+//     setDark(!dark);
+//     console.log(dark);
+//   }
+
+//   // { `${'dark' ? 'dark' : 'light'}` }
+// return (
+//     <div className= { `${dark ? 'dark' : 'light'}` }>
+//       Lesson02
+
+//       {
+//         count
+//       }
+//       <button onClick={handleCount}>increase</button>
+//       <button onClick={on}>decrease</button>
+//       <button onClick={darkNot}>dark</button>
+//     </div>
+//   )
+// }
+
+// export default Lesson02;
+
+
+import { Component } from "react";
+
+class ExampleClass extends Component {
+  constructor () {
+    super ();
+    this.state = {
+      count: 0,
+    }
+    this.handleClick = this.addCount.bind(this);
   }
 
-  const on = () => {
-    setCount(count - 1);
+  addCount() {
+    this.setState((prevState) => ({
+      count: prevState.count-1
+    }))
   }
 
-  const darkNot = () => {
-    setDark(!dark);
-    console.log(dark);
+  render() {
+    return (
+      <div>
+        Example Class
+
+          {this.state.count}
+          <button onClick={this.handleClick}>Add me</button>
+      </div>
+    )
   }
-
-  // { `${'dark' ? 'dark' : 'light'}` }
-return (
-    <div className= { `${dark ? 'dark' : 'light'}` }>
-      Lesson02
-
-      {
-        count
-      }
-      <button onClick={handleCount}>increase</button>
-      <button onClick={on}>decrease</button>
-      <button onClick={darkNot}>dark</button>
-    </div>
-  )
 }
 
-export default Lesson02;
+
+export default ExampleClass;
